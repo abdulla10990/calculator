bb98f1a2116d12e2d9e6fb888726832c0188a2d2
abdulla10990 <158803357+abdulla10990@users.noreply.github.com>
2024-03-26 13:22:37 +0400

    my cdoe

---
+++ index.js
@@ -0,0 +1,13 @@
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+function clearNum() {
+    console.log("clear");
+    document.getElementById("result").value = "";
+}
+function run(){
+    let exp = document.getElementById("result").value
+    let result = eval(exp);
+    document.getElementById("result").value = result;
+}
